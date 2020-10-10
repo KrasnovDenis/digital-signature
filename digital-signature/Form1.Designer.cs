3368d5584d2d10297b2e5d66c97be9c796bb5283
KrasnovDenis <Elo.1972@yandex.ru>
2020-10-10 22:03:46 +0400

    UI added

--- digital-signature/Form1.Designer.cs
+++ digital-signature/Form1.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.file = new System.Windows.Forms.Button();
+            this.create = new System.Windows.Forms.Button();
+            this.load = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.status = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 11);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(362, 116);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // file
+            // 
+            this.file.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.file.Location = new System.Drawing.Point(10, 133);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(105, 30);
+            this.file.TabIndex = 1;
+            this.file.Text = "File";
+            this.file.UseVisualStyleBackColor = true;
+            this.file.Click += new System.EventHandler(this.file_Click);
+            // 
+            // create
+            // 
+            this.create.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.create.Location = new System.Drawing.Point(10, 175);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(105, 30);
+            this.create.TabIndex = 2;
+            this.create.Text = "Создать ЭЦП";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // load
+            // 
+            this.load.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load.Location = new System.Drawing.Point(121, 175);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(251, 30);
+            this.load.TabIndex = 3;
+            this.load.Text = "Загрузить ЭЦП";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(118, 230);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(43, 13);
+            this.status.TabIndex = 5;
+            this.status.Text = "Status :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 301);
+            this.ClientSize = new System.Drawing.Size(386, 252);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.file);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button file;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- digital-signature/Form1.cs
+++ digital-signature/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,84 @@ namespace digital_signature
         {
             InitializeComponent();
         }
+		DSAParameters dsaparams;
+		byte[] signaturebytes;
+
+		private void file_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+
+            richTextBox1.Text =  System.IO.File.ReadAllText(filename);
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+			//get original message as byte array
+			byte[] messagebytes = Encoding.UTF8.GetBytes(
+				richTextBox1.Text);
+
+			//create digest of original message using SHA1
+			SHA1 sha1 = new SHA1CryptoServiceProvider();
+			byte[] hashbytes =
+				sha1.ComputeHash(messagebytes);
+
+			//display hash bytes in hex format
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < hashbytes.Length; i++)
+			{
+				sb.Append(String.Format(
+					"{0,2:X2} ", hashbytes[i]));
+			}
+			
+
+			//create DSA object using default key
+			DSACryptoServiceProvider dsa =
+				new DSACryptoServiceProvider();
+
+			//sign hash using OID for SHA-1
+			signaturebytes =
+				dsa.SignHash(hashbytes, "1.3.14.3.2.26");
+
+			//provide DSA parameters to verification
+			dsaparams = dsa.ExportParameters(false);
+
+			
+		}
+
+        private void load_Click(object sender, EventArgs e)
+        {
+			//get possibly modified message as byte array
+			byte[] messagebytes = Encoding.UTF8.GetBytes(
+				richTextBox1.Text);
+
+			//create digest of original message using SHA1
+			SHA1 sha1 = new SHA1CryptoServiceProvider();
+			byte[] hashbytes =
+				sha1.ComputeHash(messagebytes);
+
+			//create DSA object using parameters from signing
+			DSACryptoServiceProvider dsa =
+				new DSACryptoServiceProvider();
+			dsa.ImportParameters(dsaparams);
+
+			//do verification on hash using OID for SHA-1
+			bool match = dsa.VerifyHash(
+				hashbytes, "1.3.14.3.2.26", signaturebytes);
+
+			//show message box with result of verification
+			String strResult;
+			if (match)
+				strResult = "VerifySignature returned TRUE";
+			else
+				strResult = "VerifySignature returned FALSE";
+			MessageBox.Show(
+				strResult,
+				"Result From Calling VerifySignature",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Exclamation);
+
+		}
     }
 }
